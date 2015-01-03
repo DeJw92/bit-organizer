fc0ab94c9c1526b6f1b93ea59e14852af82402e7
Rafał Cymerys <rafal.cym@gmail.com>
2015-01-03 18:12:13 +0100

    Added methods to Team Recruitment AR

--- projects-management/src/main/java/pl/edu/knbit/organizer/TeamRecruitment.java
+++ projects-management/src/main/java/pl/edu/knbit/organizer/TeamRecruitment.java
@@ -1,7 +1,9 @@
 package pl.edu.knbit.organizer;
 
+import com.google.common.base.Preconditions;
 import org.axonframework.eventsourcing.annotation.AbstractAnnotatedAggregateRoot;
 import org.axonframework.eventsourcing.annotation.AggregateIdentifier;
+import pl.edu.knbit.organizer.events.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +19,56 @@ public class TeamRecruitment extends AbstractAnnotatedAggregateRoot {
     }
 
     public TeamRecruitment(TeamRecruitmentId id, List<MemberId> appliedMembers, TeamRecruitmentStatus status) {
+        // TODO Apply event instead?
         this.id = id;
         this.appliedMembers = appliedMembers;
         this.status = status;
+    }
+
+    public void appointMeeting(MemberId memberId) {
+        Preconditions.checkState(status != TeamRecruitmentStatus.FINISHED);
+
+        apply(new MeetingAppointedEvent(id, memberId));
+    }
+
+    public void applyForProject(MemberId memberId) {
+        Preconditions.checkState(status == TeamRecruitmentStatus.OPEN);
+
+        appliedMembers.add(memberId);
+        apply(new MemberAppliedEvent(id, memberId));
+    }
+
+    public void acceptMember(MemberId memberId) {
+        Preconditions.checkNotNull(memberId);
+        Preconditions.checkArgument(appliedMembers.contains(memberId));
+        Preconditions.checkState(status != TeamRecruitmentStatus.FINISHED);
+
+        appliedMembers.remove(memberId);
+        apply(new MemberAcceptedEvent(id, memberId));
+    }
+
+    public void rejectMember(MemberId memberId) {
+        Preconditions.checkNotNull(memberId);
+        Preconditions.checkArgument(appliedMembers.contains(memberId));
+        Preconditions.checkState(status != TeamRecruitmentStatus.FINISHED);
+
+        appliedMembers.remove(memberId);
+        apply(new MemberRejectedEvent(id, memberId));
+    }
+
+    public void closeRecruitment() {
+        Preconditions.checkState(status == TeamRecruitmentStatus.OPEN);
+
+        status = TeamRecruitmentStatus.CLOSED;
+        // TODO Do we want to send this event?
+        apply(new TeamRecruitmentClosedEvent(id));
+    }
+
+    public void finishRecruitment() {
+        // TODO Can we finish recruitment that wasn't closed?
+        Preconditions.checkState(status != TeamRecruitmentStatus.FINISHED);
+
+        status = TeamRecruitmentStatus.FINISHED;
+        apply(new TeamRecruitmentFinishedEvent(id));
     }
 }
