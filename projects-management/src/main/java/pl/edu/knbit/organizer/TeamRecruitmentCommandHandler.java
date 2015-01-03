acb50e1d1c29294e1732f9534f94ae58c7114700
Rafał Cymerys <rafal.cym@gmail.com>
2015-01-03 19:03:51 +0100

    Added team recruitment command handler.

---
+++ projects-management/src/main/java/pl/edu/knbit/organizer/TeamRecruitmentCommandHandler.java
@@ -0,0 +1,49 @@
+package pl.edu.knbit.organizer;
+
+import org.axonframework.commandhandling.annotation.CommandHandler;
+import org.axonframework.repository.Repository;
+import pl.edu.knbit.organizer.commands.*;
+
+public class TeamRecruitmentCommandHandler {
+
+    // TODO Add DI
+    private Repository<TeamRecruitment> repository;
+
+    @CommandHandler
+    public void handleAppointMeetingCommand(AppointMeetingCommand command) {
+        // TODO Should we validate if meeting can be appointed (permissions etc.)?
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.appointMeeting(command.getMemberId());
+    }
+
+    @CommandHandler
+    public void handleApplyForProjectCommand(ApplyForProjectCommand command) {
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.applyForProject(command.getMemberId());
+    }
+
+    @CommandHandler
+    public void handleAcceptMemberCommand(AcceptMemberCommand command) {
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.acceptMember(command.getMemberId());
+    }
+
+    @CommandHandler
+    public void handleRejectMemberCommand(RejectMemberCommand command) {
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.rejectMember(command.getMemberId());
+    }
+
+    @CommandHandler
+    public void handleCloseTeamRecruitmentCommand(CloseTeamRecruitmentCommand command) {
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.closeRecruitment();
+    }
+
+    @CommandHandler
+    public void handleFinishTeamRecruitmentCommand(FinishTeamRecruitmentCommand command) {
+        final TeamRecruitment teamRecruitment = repository.load(command.getTeamRecruitmentId());
+        teamRecruitment.finishRecruitment();
+    }
+
+}
